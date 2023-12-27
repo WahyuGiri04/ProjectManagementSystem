b606e692c64cc3d446fca82db0a1612f916398a0
Giri <88410452+WahyuGiri04@users.noreply.github.com>
2023-12-27 13:28:27 +0700

    update-ocelot-config

--- ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
+++ ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3391323aabb31093684dbadcddeca29e5426aed7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eedb31a94a6d357a2f432c9f9b75beb668e82cef")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
